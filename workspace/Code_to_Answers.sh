9ef5faffa9fc48019866b7d71832534823d196e8
dmerkurjev <36778179+dmerkurjev@users.noreply.github.com>
2025-08-08 11:10:51 -0700

    Update Code_to_Answers.sh

--- workspace/Code_to_Answers.sh
+++ workspace/Code_to_Answers.sh
@@ -16,16 +16,19 @@ ans_q2 <- unname(libsize["H460_L1"])+ unname(libsize["H460_L2"])
 # q3: How many genes have nonzero counts in sample siRNA_PC-3_siDICER1_Rep1_L1?
 ans_q3 <- sum(counts[, "ym"] > 0)
 
-# q4: How many genes are upregulated \uc0\u8805 2-fold (log2FC \u8805  1) in siRNA_ECC-1_NT_Rep1 vs. siRNA_PC-3_siDICER1_Rep1_L1 with FDR < 0.01?
-res_df <- as.data.frame(res)
-ans_q4 <- sum(res_df$log2FoldChange >= 1 & res_df$padj < 0.01, na.rm = TRUE)
-
-# q5: Which gene is ranked 4th by log2 fold change (most upregulated) in siRNA_ECC-1_NT_Rep1 vs. siRNA_PC-3_siDICER1_Rep1_L1?
+# q4: Which gene is the 1st deregulated by log2 fold change (most upregulated) for ECC-1??
 res_df$SYMBOL <- rownames(res_df)
 up_rank <- res_df %>%
   dplyr::filter(!is.na(log2FoldChange) & log2FoldChange > 0) %>%
   dplyr::arrange(dplyr::desc(log2FoldChange), padj, pvalue)
-ans_q5 <- if (nrow(up_rank) >= 4) up_rank$SYMBOL[3] else NA_character_
+ans_q5 <- if (nrow(up_rank) >= 1) up_rank$SYMBOL[3] else NA_character_
+
+# q5: Which gene is the 2nd deregulated by log2 fold change (most upregulated) for PC-3?
+res_df$SYMBOL <- rownames(res_df)
+up_rank <- res_df %>%
+  dplyr::filter(!is.na(log2FoldChange) & log2FoldChange > 0) %>%
+  dplyr::arrange(dplyr::desc(log2FoldChange), padj, pvalue)
+ans_q5 <- if (nrow(up_rank) >= 2) up_rank$SYMBOL[3] else NA_character_
 
 answers <- tibble::tibble(
   id = c("q1","q2","q3","q4","q5"),
